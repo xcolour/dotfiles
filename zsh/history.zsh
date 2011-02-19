f5645a70e2edfd920ff3cceb80b301a7d4aa05f3
Matt Singleton <matthew.j.singleton@gmail.com>
2011-02-19 00:03:48 -0500

    optimize history rules

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,18 +1,10 @@
-#TODO: optimize these rules. they are redundant
+#done
 
 ## Command history configuration
 HISTFILE=$HOME/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt hist_ignore_dups # ignore duplication command history list
-setopt share_history # share command history data
-
-setopt hist_verify
-setopt inc_append_history
-setopt extended_history
-setopt hist_expire_dups_first
-setopt hist_ignore_space
-
-setopt SHARE_HISTORY
-setopt APPEND_HISTORY
+setopt hist_ignore_all_dups # ignore dups including non-sequential ones
+setopt share_history # share command history data between sessions
+setopt hist_verify # load hist into command buffer rather than exec immediately
