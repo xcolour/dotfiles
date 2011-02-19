658a70c8316fdecc0047a78ca7515ea28ec7959c
Matt Singleton <matthew.j.singleton@gmail.com>
2011-02-18 19:21:35 -0500

    removing unused git functions

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,18 +1,5 @@
 #done
 
-function parse_git_branch {
-    echo "${ref#refs/heads/}"
-}
-
-function parse_git_staged {
-    $(git diff-index --quiet --cached HEAD) && return
-    echo "staged"
-}
-function parse_git_unstaged {
-    $(git diff-files --quiet) && return
-    echo "unstaged"
-}
-
 function git_prompt_info {
     ref=$(git symbolic-ref HEAD 2> /dev/null) || return
     echo "(${ref#refs/heads/}) "
