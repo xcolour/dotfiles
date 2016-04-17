03addf9245b913d8abd05750a9cc6b1f62f0db64
Matt Singleton <matt@xcolour.net>
2016-04-17 17:51:21 -0400

    get rid of slate and update vundle on every deploy run

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ rm -rf dotfiles-backup
 mkdir -p dotfiles-backup
 
 # deploy
-for f in vimrc gvimrc gitconfig gitignore zshrc zsh dircolors.conf slate tmux.conf hammerspoon
+for f in vimrc gvimrc gitconfig gitignore zshrc zsh dircolors.conf tmux.conf hammerspoon
 do
     if [ -e ~/.${f} ]; then
         mv ~/.${f} dotfiles-backup/${f}
@@ -16,8 +16,12 @@ done
 
 if [ ! -d ~/.vim/bundle/vundle ]; then
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    cd ~/.vim/bundle/vundle
+    git pull
+    cd -
 fi
 vim +BundleInstall +qall
+vim +BundleClean +qall
 
 if [ ! -e ~/.zshrc-local ]; then
     cp zshrc-local ~/.zshrc-local
